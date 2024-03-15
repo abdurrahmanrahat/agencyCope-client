08b0b8bf4e1d4da6a6f27663cf1e75a93575efc0
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-15 23:23:44 +0600

    about page last section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,13 @@
 import Section1 from "./Section1";
 import Section2 from "./Section2";
+import Section3 from "./Section3";
 
 const About = () => {
   return (
     <div>
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 };
