3bdfce223009c4a6d49e361bd0c0fbf108bbcff1
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-29 19:51:07 +0600

    count up section

--- src/Pages/Services/ServicesThings/CountingUp.jsx
+++ src/Pages/Services/ServicesThings/CountingUp.jsx
@@ -3,31 +3,39 @@ import CountUp from "react-countup";
 const CountingUp = () => {
   return (
     <div className="bg-[#f4f4f4]">
-      <div className="pb-14 max-w-screen-xl mx-auto">
-        <div className="py-14 p-12 md:px-24 grid grid-cols-1 md:grid-cols-4 gap-12 text">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="py-12 lg:py-16 md:px-24 grid grid-cols-1 md:grid-cols-4 gap-12 text">
           <div className="flex flex-col p-4 items-center justify-center">
-            <h4 className="text-[48px]">
+            <h4 className="font-extrabold text-[32px] 2xl:text-[36px] text-[#16191e]">
               <CountUp start={1} end={240} duration={3} />+
             </h4>
-            <h2 className="text-[18px] mb-2">Happy Clients</h2>
+            <h2 className="text-[#909090] lg:text-[18px] font-medium">
+              Happy Clients
+            </h2>
           </div>
           <div className="flex flex-col p-4 items-center justify-center">
-            <h4 className="text-[48px]">
-              <CountUp start={1} end={7} duration={3} />+
+            <h4 className="font-extrabold text-[32px] 2xl:text-[36px] text-[#16191e]">
+              <CountUp start={1} end={8} duration={3} />+
             </h4>
-            <h2 className="text-[18px] mb-2">Years Experience</h2>
+            <h2 className="text-[#909090] lg:text-[18px] font-medium">
+              Years Experience
+            </h2>
           </div>
           <div className="flex flex-col p-4 items-center justify-center">
-            <h4 className="text-[48px]">
+            <h4 className="font-extrabold text-[32px] 2xl:text-[36px] text-[#16191e]">
               <CountUp start={1} end={32} duration={3} />+
             </h4>
-            <h2 className="text-[18px] mb-2">Reviews</h2>
+            <h2 className="text-[#909090] lg:text-[18px] font-medium">
+              Reviews
+            </h2>
           </div>
           <div className="flex flex-col p-4 items-center justify-center">
-            <h4 className="text-[48px]">
+            <h4 className="font-extrabold text-[32px] 2xl:text-[36px] text-[#16191e]">
               <CountUp start={1} end={13} duration={3} />+
             </h4>
-            <h2 className="text-[18px] mb-2">Countries</h2>
+            <h2 className="text-[#909090] lg:text-[18px] font-medium">
+              Countries
+            </h2>
           </div>
         </div>
       </div>
