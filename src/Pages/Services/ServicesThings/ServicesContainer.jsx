15031242e138624200322cce30a4d294c3482231
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-30 22:32:28 +0600

    services container list design

--- src/Pages/Services/ServicesThings/ServicesContainer.jsx
+++ src/Pages/Services/ServicesThings/ServicesContainer.jsx
@@ -1,15 +1,15 @@
-import { IoIosArrowRoundForward } from "react-icons/io";
+// import { IoIosArrowRoundForward } from "react-icons/io";
 import Image from "../../../assets/services/click-container-img.png";
 
 const ServicesContainer = () => {
   return (
     <div className="bg-[#16191e] text-[#f4f4f4]">
       <div className="max-w-screen-xl mx-auto">
-        <div className="mx-4 md:mx-0 py-10">
+        <div className="mx-4 md:mx-0 py-16">
           {/* head section */}
           <div className="flex flex-col justify-center items-center">
             <img src={Image} className="w-40" alt="" />
-            <h4 className="text-[24px] lg:text-[32px] font-medium mt-4">
+            <h4 className="text-[24px] lg:text-[32px] font-medium text-center mt-4">
               Here’s how we can work together!
             </h4>
             <p className="text-[16px] text-center max-w-[50ch] mt-2">
@@ -23,9 +23,8 @@ const ServicesContainer = () => {
             {/* Item one */}
             <div>
               <a href="#item-one">
-                <span className="border-b-2 border-[#ffe600] hover:border-transparent duration-700 text-[20px] md:text-[24px]">
-                  BRAND IDENTITY DESIGN{" "}
-                  <IoIosArrowRoundForward className="inline-block text-[36px] font-bold" />
+                <span className="border-b-2 lg:text-[18px] font-medium border-[#ffe600] text-[#ffe600] shadow-class bg-[#16191E] rounded-md p-[6px]">
+                  BRAND IDENTITY
                 </span>
               </a>
             </div>
@@ -33,9 +32,8 @@ const ServicesContainer = () => {
             {/* Item two */}
             <div>
               <a href="#item-two">
-                <span className="border-b-2 border-[#ffe600] hover:border-transparent duration-700 text-[20px] md:text-[24px]">
-                  Website-In-A-Day{" "}
-                  <IoIosArrowRoundForward className="inline-block text-[36px] font-bold" />
+                <span className="border-b-2 lg:text-[18px] font-medium border-[#ffe600] text-[#ffe600] shadow-class bg-[#16191E] rounded-md p-[6px] uppercase">
+                  Website Design
                 </span>
               </a>
             </div>
@@ -43,9 +41,8 @@ const ServicesContainer = () => {
             {/* item three */}
             <div>
               <a href="#item-three">
-                <span className="border-b-2 border-[#ffe600] hover:border-transparent duration-700 text-[20px] md:text-[24px]">
-                  GRAPHIC DESIGN{" "}
-                  <IoIosArrowRoundForward className="inline-block text-[36px] font-bold" />
+                <span className="border-b-2 lg:text-[18px] font-medium border-[#ffe600] text-[#ffe600] shadow-class bg-[#16191E] rounded-md p-[6px]">
+                  GRAPHIC DESIGN
                 </span>
               </a>
             </div>
@@ -53,10 +50,8 @@ const ServicesContainer = () => {
             {/* item four */}
             <div>
               <a href="#item-four">
-                {" "}
-                <span className="border-b-2 border-[#ffe600] hover:border-transparent duration-700 text-[20px] md:text-[24px]">
-                  DIGITAL MARKETING{" "}
-                  <IoIosArrowRoundForward className="inline-block text-[36px] font-bold" />
+                <span className="border-b-2 lg:text-[18px] font-medium border-[#ffe600] text-[#ffe600] shadow-class bg-[#16191E] rounded-md p-[6px]">
+                  DIGITAL MARKETING
                 </span>
               </a>
             </div>
@@ -64,18 +59,16 @@ const ServicesContainer = () => {
             {/* item five */}
             <div>
               <a href="#item-five">
-                <span className="border-b-2 border-[#ffe600] hover:border-transparent duration-700 text-[20px] md:text-[24px]">
+                <span className="border-b-2 lg:text-[18px] font-medium border-[#ffe600] text-[#ffe600] shadow-class bg-[#16191E] rounded-md p-[6px] uppercase">
                   Social Media Content
-                  <IoIosArrowRoundForward className="inline-block text-[36px] font-bold" />
                 </span>
               </a>
             </div>
 
             {/* item six */}
             <div>
-              <span className="border-b-2 border-[#ffe600] hover:border-transparent duration-700 text-[20px] md:text-[24px]">
-                BRAND IDENTITY DESIGN{" "}
-                <IoIosArrowRoundForward className="inline-block text-[36px] font-bold" />
+              <span className="border-b-2 lg:text-[18px] font-medium border-[#ffe600] text-[#ffe600] shadow-class bg-[#16191E] rounded-md p-[6px]">
+                BRAND IDENTITY DESIGN
               </span>
             </div>
           </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,7 +4,6 @@ import About from "../Pages/About/About";
 import Contact from "../Pages/Contact/Contact";
 import Home from "../Pages/Home/Home/Home";
 import ServicePage from "../Pages/Services/ServicePage/ServicePage";
-import Services from "../Pages/Services/Services";
 import ScrollToTop from "../components/ui/ScrollToTop";
 
 const router = createBrowserRouter([
@@ -31,10 +30,6 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About></About>,
-      },
-      {
-        path: "demo",
-        element: <Services></Services>,
       },
     ],
   },
