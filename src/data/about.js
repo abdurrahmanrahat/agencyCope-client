a229b8af5998c1d7759b50cdb7c1a76db29abf8f
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-07-28 23:08:03 +0600

    modify about tools

--- src/data/about.js
+++ src/data/about.js
@@ -1,16 +1,27 @@
 import illustrator from "../assets/about/ai.webp";
 import figma from "../assets/about/figma.webp";
 import photoshop from "../assets/about/ps.webp";
+import expressjs from "../assets/about/tools/express.png";
+import framer from "../assets/about/tools/framer.webp";
+import mongodb from "../assets/about/tools/mongodb.png";
+import nextjs from "../assets/about/tools/next.png";
+import nodejs from "../assets/about/tools/nodejs.png";
+import reactjs from "../assets/about/tools/react.png";
+import webflow from "../assets/about/tools/webflow.webp";
+import wordpress from "../assets/about/tools/wordpress.webp";
 
 export const tools = [
   { name: "Photoshop", tag: "Design Tool", icon: photoshop },
   { name: "Illustrator", tag: "Design Tool", icon: illustrator },
-  { name: "Framer", tag: "Design Tool", icon: photoshop },
-  { name: "Webflow", tag: "Design Tool", icon: figma },
+  { name: "Framer", tag: "Design Tool", icon: framer },
+  { name: "Webflow", tag: "Design Tool", icon: webflow },
   { name: "Figma", tag: "Design Tool", icon: figma },
-  { name: "Angular", tag: "Dev Tool", icon: photoshop },
-  { name: "VueJS", tag: "Dev Tool", icon: photoshop },
-
+  { name: "ReactJS", tag: "Dev Tool", icon: reactjs },
+  { name: "NextJS", tag: "Dev Tool", icon: nextjs },
+  { name: "Wordpress", tag: "Dev Tool", icon: wordpress },
+  { name: "NodeJS", tag: "Dev Tool", icon: nodejs },
+  { name: "ExpressJS", tag: "Dev Tool", icon: expressjs },
+  { name: "MongoDB", tag: "Dev Tool", icon: mongodb },
 ];
 
 
